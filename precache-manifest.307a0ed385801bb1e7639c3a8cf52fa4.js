399fa9a3d390d40cbc7a8d67f5588c277a09b4cc
Evgeny Grigorev <agron@live.ru>
2020-01-29 19:53:49 +0500

    Updates

---
+++ precache-manifest.307a0ed385801bb1e7639c3a8cf52fa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f08486adea96fc2562b4bcd2fda3e8",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "1b708b234ce4b49eef89",
+    "url": "/Calculator/static/css/main.8aead27e.chunk.css"
+  },
+  {
+    "revision": "224ad955df91a1b78d0f",
+    "url": "/Calculator/static/js/2.c9d5abbc.chunk.js"
+  },
+  {
+    "revision": "1b708b234ce4b49eef89",
+    "url": "/Calculator/static/js/main.70132190.chunk.js"
+  },
+  {
+    "revision": "0476f8ab518e3b3f410a",
+    "url": "/Calculator/static/js/runtime-main.48d81177.js"
+  }
+]);
